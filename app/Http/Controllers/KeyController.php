daaba3e3a0820f0ac3e0074fdb76ae2b1db079d7
anders <anders@webbsallad.se>
2024-10-02 15:12:55 +0200

    Har lyckats hämta access token.

--- app/Http/Controllers/KeyController.php
+++ app/Http/Controllers/KeyController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class KeyController extends Controller
 {
     public function index()
     {
-       //$artists = Artist::latest()->limit(10)->get();
-        //$totalartists = Artist::all();
-        $token = "bertil";
+        $token = Http::withHeaders(['Content-Type'=> 'application/x-www-form-urlencoded'
+        ])->asForm()->post('https://accounts.spotify.com/api/token', [
+            'grant_type'=> 'client_credentials',
+            'client_id'=> '6eded995112346d8aa49254b94d31e5b',
+            'client_secret'=> '4f33f9955a6748e0b678f94d57bed679'
+        ]);
 
         return view('/keycall')->with('token', $token);
     }
