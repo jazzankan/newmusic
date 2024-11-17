005e3a4beeb12c0aea634870507bc175ff88c510
anders <anders@webbsallad.se>
2024-11-17 19:02:10 +0100

    Funktionen för att addera items till albumen funkar inte som tänkt.

--- app/Livewire/SearchAlbums.php
+++ app/Livewire/SearchAlbums.php
@@ -11,19 +11,41 @@ use Illuminate\Support\Facades\Storage;
 class SearchAlbums extends Component
 {
     public $token;
-    public $response;
-    public function getalbums(){
+    public $artAlbArray = [];
+
+    public function getall($token, $item, $album_resp)
+    {
+        $next_response = Http::withHeaders(['Authorization' => 'Bearer ' . $token
+        ])->get(url: $album_resp["next"] . $item->spotify_id . '/albums')->json();
+        $album_resp = array_merge($album_resp, $next_response);
+        if (isset($album_resp["next"])) {
+            $this->getall($token, $item, $album_resp);
+        }
+        return $album_resp;
+    }
+
+    public function getalbums()
+    {
         $artists = Artists::all();
         $lasttime = file_get_contents("../lasttime.txt");
         $lasttime = str_replace("\n", "", $lasttime);
 
         $this->token = (new KeyController)->index();
         $this->token = $this->token['access_token'];
+
         $artists->each(function ($item) {
-        $this->response = Http::withHeaders(['Authorization'=> 'Bearer '.$this->token
-        ])->get(url:'https://api.spotify.com/v1/artists/' . $item->id)->json();
-        dd($item->id);
+            $art_response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->token
+            ])->get(url: 'https://api.spotify.com/v1/artists/' . $item->spotify_id)->json();
+            $albums_response = Http::withHeaders(['Authorization' => 'Bearer ' . $this->token
+            ])->get(url: 'https://api.spotify.com/v1/artists/' . $item->spotify_id . '/albums')->json();
+            if (isset($albums_response["next"])) {
+                $albums_response = $this->getall($this->token, $item, $albums_response);
+            }
+            $this->artAlbArray[$art_response["name"]] = $albums_response["items"];
+
         });
+
+        //dd($this->artAlbArray);
     }
 
 // Ellen id: 5N87utqQzCT8NHBW7JJXog
--- resources/views/livewire/search-albums.blade.php
+++ resources/views/livewire/search-albums.blade.php
@@ -1,6 +1,14 @@
 <div>
     <p wire:click.prevent="getalbums" class="mb-4"><a class="btn-blue" href="">Sök nya</a></p>
-    @if($response)
-    <p>{{ $response["items"][17]["name"] }}</p>
+    @if($artAlbArray)
+    @foreach($artAlbArray as $key => $value)
+<b>{{ $key }}:</b><br>
+        @foreach($value as $v)
+            @if($v["release_date"] > 1950-01-01)
+        {{ $v["name"] }}<br>
+                @endif
+            @endforeach
+        <p>---</p>
+    @endforeach
         @endif
 </div>
